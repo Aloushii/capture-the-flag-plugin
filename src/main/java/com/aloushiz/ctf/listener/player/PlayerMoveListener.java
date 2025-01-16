2b5c78869921041a858c66982608f857c2eef4bf
Ali <sabsabialioutlook.com>
2025-01-15 21:01:34 -0500

    Created empty body PlayerMoveListener

---
+++ src/main/java/com/aloushiz/ctf/listener/player/PlayerMoveListener.java
@@ -0,0 +1,29 @@
+package com.aloushiz.ctf.listener.player;
+
+import com.aloushiz.ctf.CaptureTheFlag;
+import com.aloushiz.ctf.game.Game;
+import com.aloushiz.ctf.game.GameState;
+import com.aloushiz.ctf.team.Team;
+import com.aloushiz.ctf.team.TeamManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import java.util.List;
+
+public class PlayerMoveListener implements Listener {
+/*
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){
+        Player player = event.getPlayer();
+        Game game = CaptureTheFlag.getGame();
+
+        if (game.getGameState().equals(GameState.IN_GAME)){
+            List<Team> teamList = TeamManager.getTeamList();
+            for (Team team : teamList){
+                for ()
+            }
+        }
+    }*/
+}
